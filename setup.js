dc93aa6021ec2ddb9ce8b97920f5bdd4577c54ef
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-26 00:20:11 -0600

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,24 @@
+// step 1
+
+function setup (arr) {
+  var friends = new Object();
+
+  arr.forEach((pair) => addPair(friends, pair));
+
+  return friends;
+}
+
+function addPair (friends, pair) {
+  let user = pair[0];
+  let otherUser = pair[1];
+  addFriends(friends, user, otherUser);
+  addFriends(friends, otherUser, user);
+}
+
+function addFriends (friends, user, otherUser) {
+  if (user in friends) {
+    friends[user].push(otherUser);
+  } else {
+    friends[user] = [otherUser];
+  }
+}
