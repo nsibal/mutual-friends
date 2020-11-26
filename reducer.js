f7a5059bf1959bfb332f29c0f224e11cf99bafe9
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-26 00:44:52 -0600

    Create reducer.js

---
+++ reducer.js
@@ -0,0 +1,10 @@
+// step 4
+
+function reducer (groupedObj) {
+  for (var pair in groupedObj) {
+    let first = groupedObj[pair][0];
+    let second = groupedObj[pair][1];
+
+    groupedObj[pair] = first.filter(user => second.includes(user));
+  }
+}
